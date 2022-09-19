006bd144914cfc23b57c893f167c3e826078acc7
traxys <quentin+dev@familleboyer.net>
2022-09-19 16:44:30 +0200

    Add dbg

--- winit/src/application.rs
+++ winit/src/application.rs
@@ -159,7 +159,7 @@ where
         let document = window.document().unwrap();
         let body = document.body().unwrap();
 
-        let _ = match body.query_selector("#iced_root").unwrap() {
+        let _ = match dbg!(body.query_selector("#iced_root").unwrap()) {
             Some(e) => body
                 .replace_child(&canvas, &e)
                 .expect("Could not replace iced_root"),
